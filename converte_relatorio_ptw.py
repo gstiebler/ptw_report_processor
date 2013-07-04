3363f838c011450af82dc77257b09a3fb5168363
Guilherme Stiebler <guilhermemst@gmail.com>
2013-07-03 21:44:05 -0300

    gerando excel pra cada relatório

--- converte_relatorio_ptw.py
+++ converte_relatorio_ptw.py
@@ -1,17 +1,20 @@
 
 import os
+from xlsxwriter.workbook import Workbook
 
-
-def process_report( report_file_name ):
+def process_report( dir_reports, file_name ):
+    
+    report_file_name = dir_reports + "/" + file_name
 
     # pega somente o nome do arquivo sem a extensao .RPT
-    file_name = report_file_name.split('.')[0]
+    file_name_we = file_name.split('.')[0]
 
     # abre o arquivo de entrada
     report_file = open(report_file_name, "r")
 
     # abre o arquivo de saida
-    output_file = open( file_name + ".csv", "w" )
+    workbook = Workbook(file_name_we + ".xlsx")
+    worksheet = workbook.add_worksheet()
 
     # pega as linhas do arquivo
     report_lines = report_file.readlines()
@@ -20,18 +23,18 @@ def process_report( report_file_name ):
     num_lines = len(report_lines)
 
     i = 0
+    output_line = 0
     while i < num_lines:
         # pega a iesima linha do relatorio
         line = report_lines[i]
         # verifica se a linha contem '*FAULT BUS:'
         if line.find('*FAULT BUS:') > 0:
-            output_line = ""
             # separa as palavras da linha
             line_parts = line.split()
             # o nome do painel eh a terceira palavra da linha
             panel_name = line_parts[2]
             # adiciona o painel na linha de saida
-            output_line += panel_name + ";"
+            worksheet.write( output_line, 0,  panel_name )
             # pula 5 linhas
             i += 5
             # pega a linha do relatorio com os valores
@@ -43,15 +46,16 @@ def process_report( report_file_name ):
                 break
             # percorre os valores da linha. O primeiro valor eh a 4a palavra
             for n in range(3, 8):
-                output_line += line_parts[n] + ";" # adiciona o valor na linha de saida
+                worksheet.write( output_line, n - 2, line_parts[n] )
                 
-            # joga a linha de saida pra dentro do arquivo de saida	
-            output_file.write( output_line + "\n" )
+            output_line += 1
+            
         # passa pra proxima linha
         i += 1
         
     # fecha o arquivo de saida
-    output_file.close()
+    
+    workbook.close()
 
 
 
@@ -60,4 +64,4 @@ dir_reports = os.path.dirname(full_path) + "/relatorios"
 
 for file in os.listdir(dir_reports):
     if file.endswith(".RPT"):
-        process_report( dir_reports + "/" + file )
+        process_report( dir_reports, file )
