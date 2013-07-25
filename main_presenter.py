3d257c3aae39223d76eedcbae4b19492eeb36b7a
Guilherme Stiebler <guilhermemst@gmail.com>
2013-07-25 20:23:03 -0300

    quebrando função

--- main_presenter.py
+++ main_presenter.py
@@ -33,27 +33,10 @@ def process_report( excel_output_folder, reports_folder, file_name ):
         line = report_lines[i]
         # verifica se a linha contem '*FAULT BUS:'
         if line.find('*FAULT BUS:') > 0:
-            # separa as palavras da linha
-            line_parts = line.split()
-            # o nome do painel eh a terceira palavra da linha
-            panel_name = line_parts[2]
-            # adiciona o painel na linha de saida
-            worksheet.write( output_line, 0,  panel_name )
-            # pula 5 linhas
-            i += 5
-            # pega a linha do relatorio com os valores
-            line = report_lines[i]
-            # divide as palavras da linha
-            line_parts = line.split()
-            
-            if len( line_parts ) < 8:
+            if process_line( worksheet, output_line, report_lines, i ) == False:
                 break
-            # percorre os valores da linha. O primeiro valor eh a 4a palavra
-            for n in range(3, 8):
-                text = line_parts[n]
-                worksheet.write( output_line, n - 2, text )
-                
             output_line += 1
+            i += 5
             
         # passa pra proxima linha
         i += 1
@@ -61,7 +44,31 @@ def process_report( excel_output_folder, reports_folder, file_name ):
     # fecha o arquivo de saida
     
     workbook.close()
-
+    
+def process_line( worksheet, output_line, report_lines, i ):
+    # pega a iesima linha do relatório
+    line = report_lines[i]
+    # separa as palavras da linha
+    line_parts = line.split()
+    # o nome do painel eh a terceira palavra da linha
+    panel_name = line_parts[2]
+    # adiciona o painel na linha de saida
+    worksheet.write( output_line, 0,  panel_name )
+    # pula 5 linhas
+    i += 5
+    # pega a linha do relatorio com os valores
+    line = report_lines[i]
+    # divide as palavras da linha
+    line_parts = line.split()
+    
+    if len( line_parts ) < 8:
+        return False
+    # percorre os valores da linha. O primeiro valor eh a 4a palavra
+    for n in range(3, 8):
+        text = line_parts[n]
+        worksheet.write( output_line, n - 2, text )
+                
+                
 class MainPresenter:
 
     def __init__( self ):
